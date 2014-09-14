5e870b1d14a0e0bf94749beaccb3d27b04ada2d1
Alexander Erm <alexerm@gmail.com>
2014-09-14 19:08:28 +0300

    Blank plugin template

---
+++ twilio-click-to-call.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Twilio Click-2-Call
+* Plugin URI: http://wordpress.org/plugins/twilio-click-to-call
+* Description: Create Twilio click to call button, connect client and your sales rep, record and analyse calls.
+* Version: 1.0
+* Author: Alexander Erm
+* Author URI: http://facebook.com/alexander/erm
+* License: A "Slug" license name e.g. GPL2
+*/
